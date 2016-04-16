0ed0a168b361cb38fa0bbf98dadcb62371de165d
Ronen Moldovan <ronen.moldovan@gmail.com>
2016-04-16 23:31:48 +0300

    Support anonymous login

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ronenm:offerjar-user-affinity',
-  version: '0.1.2',
+  version: '0.1.3',
   // Brief, one-line summary of the package.
   summary: 'Support session token login and/or link of user account to offerjar account',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
+  api.versionsFrom('1.3.1');
   api.use('ronenm:offerjar-api@1.0.0');
   api.use("underscore");
   api.use("accounts-base");
